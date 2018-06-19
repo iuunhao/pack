036ceeef7f4ffa9d354f8f3c67d1c014c2fb634c
Arvin.iuunhao <arvin.ljh@alibabapictures.com>
2018-06-20 07:11:07 +0800

    haha

---
+++ file.js
@@ -0,0 +1,14 @@
+(function() {
+	function formatDate(now) {
+		var y = now.getFullYear();
+		var m = now.getMonth() + 1; // 注意js里的月要加1
+		var d = now.getDate();
+		var h = now.getHours();
+		var m = now.getMinutes();
+		var s = now.getSeconds();
+		return y + '-' + m + '-' + d + ' ' + h + ':' + m + ':' + s;
+	}
+	var nowDate = new Date(2016, 5, 13, 19, 18, 30, 20);
+	console.log(nowDate.getTime()); // 获得当前毫秒数: 1465816710020
+	console.log(formatDate(nowDate));
+})();
